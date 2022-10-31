d8242997a8264422748e66bd17a19d26efe8ed2c
Akcel77 <Akeucel@gmail.com>
2022-10-31 12:36:09 +0100

    MAJ Crud admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Booking;
+use App\Entity\ClosingDay;
 use App\Entity\Offer;
+use App\Entity\OpeningDay;
+use App\Entity\Reservation;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -54,6 +58,10 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
         yield MenuItem::linkToCrud('Prestations', 'fas fa-desktop', Offer::class);
+        yield MenuItem::linkToCrud('ClosingDay', 'fas fa-clock', ClosingDay::class);
+        yield MenuItem::linkToCrud('OpeningDay', 'fas fa-clock', OpeningDay::class);
+        yield MenuItem::linkToCrud('Booking', 'fas fa-clock', Booking::class);
+        yield MenuItem::linkToCrud('Reservation', 'fas fa-clock', Reservation::class);
 
     }
 }
